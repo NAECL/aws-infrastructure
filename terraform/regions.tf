ae1e25f3b1ccfda0b50f017f31712993d21f9a3e
Ian Salt <gpl@naecl.co.uk>
2019-01-03 19:44:29 +0000

    Set global configs

---
+++ terraform/regions.tf
@@ -0,0 +1,37 @@
+# US East Regions
+# region[10]  =   us-east-1      US East (N. Virginia)
+# region[11]  =   us-east-2      US East (Ohio)
+
+# US West Regions
+# region[20]  =   us-west-1      US West (N. California)
+# region[21]  =   us-west-2      US West (Oregon)
+
+# Canada Regions
+# region[30]  =   ca-central-1   Canada (Central)
+
+# Europe West Regions
+# region[40]  =   eu-west-1      EU (Ireland)
+# region[41]  =   eu-west-2      EU (London)
+# region[42]  =   eu-west-3      EU (Paris)
+
+# Europe Central Regions
+# region[50]  =   eu-central-1   EU (Frankfurt)
+
+# Asia Pacific North East Regions
+# region[60]  =   ap-northeast-1 Asia Pacific (Tokyo)
+# region[61]  =   ap-northeast-2 Asia Pacific (Seoul)
+# region[62]  =   ap-northeast-3 Asia Pacific (Osaka)
+
+# Asia Pacific South East Regions
+# region[70]  =   ap-southeast-1 Asia Pacific (Singapore)
+# region[71]  =   ap-southeast-2 Asia Pacific (Sydney)
+
+# Asia Pacific South Regions
+# region[80]  =   ap-south-1     Asia Pacific (Mumbai)
+
+# South America Regions
+# region[90]  =   sa-east-1      South America (São Paulo)
+
+# China Regions
+# region[100] =   cn-north-1     China (Beijing)
+# region[100] =   cn-northwest-1 China (Ningxia)
