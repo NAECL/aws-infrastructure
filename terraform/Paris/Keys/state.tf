a05a5d5210e3469d7aae45c1e11f6f7e03918aae
Ian Salt <gpl@naecl.co.uk>
2019-07-14 13:13:49 +0100

    Changed state bucket name format

--- terraform/Paris/Keys/state.tf
+++ terraform/Paris/Keys/state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "naecl.co.uk.terraform"
+    bucket = "terraform-public.naecl.co.uk",
     key    = "state/Keys.Paris/terraform.tfstate"
     region = "eu-west-2"
   }
