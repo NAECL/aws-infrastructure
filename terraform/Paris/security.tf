e2fc5c51c65b80c3b2748346b96ec8460e348f0d
Ian Salt <gpl@naecl.co.uk>
2019-07-14 14:20:24 +0100

    Opened Paris wordpress http/https to public

--- terraform/Paris/security.tf
+++ terraform/Paris/security.tf
@@ -50,14 +50,14 @@ resource "aws_security_group" "public_web" {
       from_port = 80
       to_port = 80
       protocol = "tcp"
-      cidr_blocks = ["${var.homeIp}"]
+      cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
       from_port = 443
       to_port = 443
       protocol = "tcp"
-      cidr_blocks = ["${var.homeIp}"]
+      cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
