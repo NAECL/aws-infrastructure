6d310d40d217e8d1dd16162d3a71edfed7eeef2e
Ian Salt <ian@naecl.co.uk>
2019-11-04 19:23:07 +0000

    Removed FS Size override, and commented out section

--- terraform/Paris/Wordpress.1/wordpress.tf
+++ terraform/Paris/Wordpress.1/wordpress.tf
@@ -10,10 +10,6 @@ resource "aws_instance" "wordpress" {
   subnet_id               = "${aws_subnet.PublicA.id}"
   vpc_security_group_ids  = [ "${aws_security_group.public_web.id}" ]
   iam_instance_profile    = "STANDARD_profile"
-  # disable_api_termination = "true"
-  root_block_device {
-    volume_size = 8
-  }
   user_data               = <<EOF
 #!/bin/bash -x
 curl http://aws.naecl.co.uk/public/build/bootstrap/install.sh | bash
