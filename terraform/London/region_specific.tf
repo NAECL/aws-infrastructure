d60a675b9f01e4cb0ae292015d89008843f204ae
Ian Salt <gpl@naecl.co.uk>
2019-11-12 23:22:44 +0000

    Moved Squid Buildto London

---
+++ terraform/London/region_specific.tf
@@ -0,0 +1,23 @@
+variable "regionSubnet" {
+  default = "10.41"
+}
+
+variable "region_name" {
+  default = "london"
+}
+
+variable "region" {
+  default = "eu-west-2"
+}
+
+variable "region_zonea" {
+  default = "eu-west-2a"
+}
+
+variable "region_zoneb" {
+  default = "eu-west-2b"
+}
+
+variable "centos-7-ami" {
+  default = "ami-0e1ab783dc9489f34"
+}
