1cecc7d4a2b0f8af5f5e0bbc07a1590fc6a6c6fa
Ian Salt <gpl@naecl.co.uk>
2019-11-12 22:11:42 +0000

    Set firewall rules for squid

--- terraform/Paris/security.tf
+++ terraform/Paris/security.tf
@@ -89,15 +89,8 @@ resource "aws_security_group" "public_squid" {
   }
 
   ingress {
-      from_port = 80
-      to_port = 80
-      protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-      from_port = 443
-      to_port = 443
+      from_port = 3128
+      to_port = 3128
       protocol = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
   }
