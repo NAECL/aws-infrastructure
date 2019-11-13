abf567794074a61c8ed2d265b44a9b0612cb24b7
Ian Salt <ian@naecl.co.uk>
2019-11-13 00:31:05 +0000

    Set proxy to only work from Home IP

--- terraform/London/security.tf
+++ terraform/London/security.tf
@@ -92,7 +92,7 @@ resource "aws_security_group" "public_squid" {
       from_port = 3128
       to_port = 3128
       protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
+      cidr_blocks = ["${var.homeIp}"]
   }
 
   egress {
