8e1bd687e9f30bca26880b3a4d8b769c7a28948d
AWS Test Account <awstest@hp-laptop.naecl.local>
2020-06-23 14:45:03 +0100

    Changed region specific info to symlink outside repo

---
+++ terraform/London/ami.tf
@@ -0,0 +1,1 @@
+../../../aws-infrastructure-local/ami.London.tf
--- terraform/London/region_specific.tf
+++ terraform/London/region_specific.tf
@@ -1,1 +1,19 @@
-../../../aws-infrastructure-local/region_specific.London.tf
+variable "regionSubnet" {
+  default = "10.41"
+}
+
+variable "region_name" {
+  default = "london"
+}
+
+variable "region" {
+  default = "eu-west-2"
+}
+
+variable "region_zonea" {
+  default = "eu-west-2a"
+}
+
+variable "region_zoneb" {
+  default = "eu-west-2b"
+}
